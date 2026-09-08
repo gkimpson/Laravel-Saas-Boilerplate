17c92900c44d007dd170d4c2156be3565b28bead
Gavin Kimpson <gkimpson@gmail.com>
2026-09-08 06:24:30 +0100

    feat: added Laravel Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
-use Laravel\Cashier\Billable;
 use Laravel\Fortify\Contracts\PasskeyUser;
 use Laravel\Fortify\PasskeyAuthenticatable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @property int $id
